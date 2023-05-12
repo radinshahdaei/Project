266d3df3031ad738073d3472ad4e6dc312ecfb15
radinshahdaei <radin.shahdaei@gmail.com>
2023-05-12 16:38:27 +0330

    battering ram

--- src/main/java/Model/Person/Military/Siege/SiegeType.java
+++ src/main/java/Model/Person/Military/Siege/SiegeType.java
@@ -12,7 +12,7 @@ public enum SiegeType {
     SIEGE_TOWER("siege tower", 5, 0, 150, 0, 4, getResources("gold", "150")),
     PORTABLE_SHIELD("portable shield", 8, 0, 100, 0, 1, getResources("gold", "5")),
     ARABIC_FIRE_BALLISTA("arabic fire ballista", 5, 50, 150, 15, 2, getResources("gold", "150")), //set fire
-    BATTERING_RAMS("battering ram", 8, 100, 150, 0, 4, getResources("gold", "200"));
+    BATTERING_RAMS("battering ram", 8, 200, 150, 0, 4, getResources("gold", "200"));
     //TODO siege tower and battering ram can have wall destination
 
 
